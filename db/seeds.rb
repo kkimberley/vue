0462217e8f609f7e36bc2aab39a7fc6fee698cc2
kimberley <kimberley@jvdiamondtech.com>
2019-09-19 01:17:08 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create! name: 'Kimberley', phone: '02-12345678', email: 'kimberley@mail.com'
+User.create! name: 'Albert', phone: '02-12345678', email: 'albert@mail.com'
+User.create! name: 'Hector', phone: '02-12345678', email: 'hector@mail.com'
+User.create! name: 'Brohand', phone: '02-12345678', email: 'brohand@mail.com'
+User.create! name: 'Linda', phone: '02-12345678', email: 'linda@mail.com'
+User.create! name: 'Erin', phone: '02-12345678', email: 'erin@mail.com'
+User.create! name: 'Leo', phone: '02-12345678', email: 'leo@mail.com'
